3470e945f8de5b79d627ebd5c2a66962d1a3437e
Couqdewit <nquocn2402@gmail.com>
2022-04-13 18:24:24 +0700

    Update

--- Vigenere Cipher/ProjectX/Form1.cs
+++ Vigenere Cipher/ProjectX/Form1.cs
@@ -20,10 +20,17 @@ namespace ProjectX
         #region Bat dau he ma Vigenere
         private void Vigenere_btnMahoa_Click(object sender, EventArgs e)
         {
-            int[] arc = Vigenere.chuyenmakey(Vigenere_txtKhoaBanro.Text);
-            int[] dongkhoa = Vigenere.taokhoa(Vigenere_txtBanro.Text, arc);
-            //string result = (arr == null) ? null : arr.Skip(1).Aggregate(arr[0].ToString(), (s, i) => s + "," + i.ToString());
-            Vigenere_txtBanma.Text = Vigenere.Mahoa(Vigenere_txtBanro.Text, dongkhoa);
+           if (Vigenere_txtBanro.Text.Length != 0)
+            {
+                int[] arc = Vigenere.chuyenmakey(Vigenere_txtKhoaBanro.Text);
+                int[] dongkhoa = Vigenere.taokhoa(Vigenere_txtBanro.Text, arc);
+                //string result = (arr == null) ? null : arr.Skip(1).Aggregate(arr[0].ToString(), (s, i) => s + "," + i.ToString());
+                Vigenere_txtBanma.Text = Vigenere.Mahoa(Vigenere_txtBanro.Text, dongkhoa);
+            }
+            else
+            {
+                MessageBox.Show("Sorry. Plaintext or Key Plaintext invalid.");return;
+            }         
         }
         private void Vigenere_btnClear_Click(object sender, EventArgs e)
         {
@@ -34,11 +41,17 @@ namespace ProjectX
         }       
         private void Vigenere_btnGiaima_Click(object sender, EventArgs e)
         {
+            if (Vigenere_txtKhoaBanma.Text.Length != 0) { 
             int[] arc = Vigenere.chuyenmakey(Vigenere_txtKhoaBanma.Text);
             int[] dongkhoa = Vigenere.taokhoa(Vigenere_txtBanma.Text, arc);
             //string result = (arr == null) ? null : arr.Skip(1).Aggregate(arr[0].ToString(), (s, i) => s + "," + i.ToString());
             Vigenere_txtBanro.Text = Vigenere.Giaima(Vigenere_txtBanma.Text, dongkhoa);
         }
+            else
+            {
+                MessageBox.Show("Sorry. Ciphertext or Key Ciphertext invalid.");return;
+            }
+}
 
         private void Vigenere_btnChonfileBanro_Click(object sender, EventArgs e)
         {
